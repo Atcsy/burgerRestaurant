638d614d1229a2f6cb59ebd5d9ff0c05f092f75a
Attila Katona <katona.dev@gmail.com>
2020-02-28 13:40:18 +0100

    separated the views into template layouts

---
+++ resources/views/menu/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="welcome-jumbo">
+      <div class="status">OLD</div>
+      <h1>Billy Burger</h1>
+      <img src="/img/main_jumbo-min.png" class="burger-fries">
+</div>
+
+@endsection
